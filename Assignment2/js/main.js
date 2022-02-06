17e2c35b0a78f64ca7e51fba272358ec0230d582
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-02-06 15:38:48 -0500

    first

--- Assignment2/js/main.js
+++ Assignment2/js/main.js
@@ -2,10 +2,41 @@
 
 
 $(function () {
+    let xhr = new XMLHttpRequest;
 
+    xhr.open('GET', 'prospect.html', true);
+    xhr.open('GET', 'convert.html', true);
+    xhr.open('GET', 'retain.html', true);
 
-    $('#prospect').click(function () {
-        $('#content').load('prospect.html');
-    });
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let prospect = document.getElementById('prospect');
+            let convert = document.getElementById('prospect');
+            let retain = document.getElementById('prospect');
+
+            $('prospect').click(function () {
+                let content = $('#content');
+                content.innerHTML = xhr.responseText;
+            });
+
+        }
+    };
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#prospect').click(function () {
+    //     $('#content').load('prospect.html');
+    // });
