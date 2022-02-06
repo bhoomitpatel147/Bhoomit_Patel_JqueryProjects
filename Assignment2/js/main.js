eb10489948c29e186a0c77abde4a2cabbf1630ab
BHOOMIT <kevinmitnick@BHOOMITs-MacBook-Pro.local>
2022-02-06 15:55:20 -0500

    1.2

--- Assignment2/js/main.js
+++ Assignment2/js/main.js
@@ -5,22 +5,24 @@ $(function () {
     let xhr = new XMLHttpRequest;
 
     xhr.open('GET', 'prospect.html', true);
-    xhr.open('GET', 'convert.html', true);
-    xhr.open('GET', 'retain.html', true);
+    //xhr.open('GET', 'convert.html', true);
+    //xhr.open('GET', 'retain.html', true);
 
     xhr.onload = function () {
         if (xhr.status === 200) {
-            let prospect = document.getElementById('prospect');
-            let convert = document.getElementById('convert');
-            let retain = document.getElementById('retaint');
+            //let prospect = document.getElementById('prospect');
+            //let convert = document.getElementById('convert');
+            //let retain = document.getElementById('retain');
 
-            $('prospect').click(function () {
-                let content = $('#content');
+            $('#prospect').click(function () {
+                let content = document.getElementById('content');
+                alert("Done");
                 content.innerHTML = xhr.responseText;
             });
 
         }
     };
+    xhr.send("search=arduino");
 
 });
 
