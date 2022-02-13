bec06a7d4df80b9e7a1d3ae97c324a29d5cb64c4
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2022-02-13 01:12:43 -0500

    Add files via upload

---
+++ Assignment3/js/script.js
@@ -0,0 +1,51 @@
+
+  $(function () {
+    getJsonObject();
+  });
+
+
+let getJsonObject = $.getJSON("team.json", function(data){
+
+    $.each(data.members, function(key, val){
+
+        $('div#team').append(`<div id="members">
+                              <h2 class="name">${val.name}</h2>
+                              <h5 class="position">${val.position}</h5>
+                              <p class="bio">${val.bio}</p>
+                              </div>`);
+    });
+
+});
+
+let delay = 3000;
+
+let ajaxMethod = $.ajax({
+    method: "GET",
+    url: "team.json",
+    dataType: "json",
+    data: "members",
+    beforeSend: function(){
+        $('div#team').append("Loading...");
+    },
+    success: function(data){
+        setTimeout(function(){
+            
+            $('div#team').empty();
+            $.each(data.members, function(key, val){
+
+                $('div#team').append(`<div id="members">
+                                    <h2 class="name">${val.name}</h2>
+                                    <h5 class="position">${val.position}</h5>
+                                    <p class="bio">${val.bio}</p>
+                                    </div>`);
+            });
+            },delay)
+    },
+    error: function (request, status, error) {
+        $('div#team').empty();
+        $('div#team').append('Error: ' + error);
+    }
+  });
+
+
+
